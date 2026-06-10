db813985b3e3ae03f40f1b00ce5c3f8d85c9acfa
ItWas271k <ethan.boudjoudi@gmail.com>
2026-06-10 14:40:08 +0200

    structure finie

--- main.h
+++ main.h
@@ -7,6 +7,23 @@ typedef  struct vector_ {
     double z;
 }vector;
 
+typedef  struct point_ {
+    vector r;
+    vector v;
+    int t;
+}p;
+
+typedef  struct trajectoire_ {
+    vector* ensemble;
+}trajectoire;
+
+typedef  struct planete_ {
+    char* nom;
+    double masse;
+    trajectoire traj;
+    double periphelie;
+}planete;
+
 vector addition(vector vector1, vector vector2);
 vector soustraction(vector vector1, vector vector2);
 vector multiplication(vector vector1, double scalaire);
