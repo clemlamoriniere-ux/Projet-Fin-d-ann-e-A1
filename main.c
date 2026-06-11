e00feab63ca797cb3d009ba65e5346287d881a8b
ItWas271k <ethan.boudjoudi@gmail.com>
2026-06-11 14:04:33 +0200

    euler 1

--- main.c
+++ main.c
@@ -7,14 +7,13 @@
 #include "trajectoire.h"
 #include "vector.h"
 
-trajectoire euler(planete planet)
-{
+trajectoire euler(planete planet){
     double x = PERIHELIE_TERRE;
     vector position = new_vector(x, 0, 0);
     // vitesse depart
 
     // valeur en Y
-    double vitesse_perihelie_y = sqrt(GRAVITY * MASSE_SOLEIL * (1 + EXCENTRISTE_TERRE)) / sqrt(DEMI_AXE_TERRE(1 - EXCENTRISTE_TERRE));
+    double vitesse_perihelie_y = sqrt((GRAVITY * MASSE_SOLEIL * (1 + EXCENTRISTE_TERRE)) / (DEMI_AXE_TERRE * (1 - EXCENTRISTE_TERRE)));
     // vitesse en vecteur
     vector vitesse = new_vector(0, vitesse_perihelie_y, 0);
     // val acc
@@ -25,11 +24,11 @@ trajectoire euler(planete planet)
     int dt = 86400;
 
     trajectoire traj;
-    traj->ensemble = malloc(sizeof(vector) * 365);
+    traj.ensemble = malloc(sizeof(point) * 365);
     for (int i = 0; i < 365; i++){
-        vector pos_t_plus_un = new_vector(addition(planet.r.x , multiplication(planet.v.x , dt)),addition(planet.r.y,multiplication(vitesse_perihelie_y,dt)),0);
-        vector v_t_plus_un = new_vector(addition(planet.v.x , multiplication(acceleration_perihelie_x , dt)),addition(planet.v.y,multiplication(0,dt)),0);
-        traj.ensemble[i] = {pos_t_plus_un, v_t_plus_un, i};
+        vector pos_t_plus_un = new_vector(addition(planet.r,multiplication(planet.v, dt)));
+        vector v_t_plus_un = new_vector(addition(planet.v,multiplication(acceleration, dt)));
+        traj.ensemble[i] = new_point(pos_t_plus_un, v_t_plus_un, i);
     }
     return traj;
 }
