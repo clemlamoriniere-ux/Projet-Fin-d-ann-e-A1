0c750b374dfe7a2d7471b49f83e76bd231fae244
ItWas271k <ethan.boudjoudi@gmail.com>
2026-06-12 11:49:34 +0200

    euler asymp mais pas fini

--- main.c
+++ main.c
@@ -35,6 +35,31 @@ int main(){
     fclose(fichier);
     printf("Fichier 'trajectoire.json' genere avec succes !\n");
     free(traj_Earth.ensemble);
+
+    planete Earth_asymp = new_planete("Terre", MASSE_TERRE, traj, PERIHELIE_TERRE);
+    trajectoire traj_Earth_asymp = euler_asymetrique(Earth);
+    printf("Euler asymètrique terminé\n");
+    FILE *fichier_asymp = fopen("trajectoire.json", "w");
+    printf("Fichier ouvert\n");
+    if (fichier_asymp == NULL){
+        printf("Erreur : Impossible de creer le fichier JSON\n");
+        return 1;
+    }
+    printf("Simulation en cours...\n");
+    fprintf(fichier_asymp, "{\n");
+    fprintf(fichier_asymp, "\"earth-euler\" : [\n");
+    printf("Debut ecriture\n");
+    for (int i = 0; i < 365 * 50; i++){
+        point p = traj_Earth_asymp.ensemble[i];
+        fprintf(fichier_asymp, "[[%e,%e,%e],[%e,%e,%e],%d]", p.r.x, p.r.y, p.r.z, p.v.x, p.v.y, p.v.z, p.t);
+        if (i < 365 * 50 - 1)
+            fprintf(fichier_asymp, ",\n");
+    }
+    fprintf(fichier_asymp, "\n]\n}");
+    fclose(fichier_asymp);
+    printf("Fichier 'trajectoire.json' genere avec succes !\n");
+    free(traj_Earth_asymp.ensemble);
     free(traj.ensemble);
+
     return 0;
 }
