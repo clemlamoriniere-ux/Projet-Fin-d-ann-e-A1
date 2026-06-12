06b8941d985064bae4cff5bd369dc8b65da25d55
clemlamoriniere-ux <clem.lamoriniere@gmail.com>
2026-06-12 14:39:40 +0200

    html et js finit

--- html/planete.js
+++ html/planete.js
@@ -1,43 +1,50 @@
 let echelle = 1000000000;
-let reader = new FileReader();
-let file;
-let dataJSON;
-let method;
-let trajectoireVisitee = [];
+let a = 0;
+let button;
+let planetes = [];
+let couleurs = [
+  [0, 0, 255],
+  [255, 100, 100],
+  [0, 255, 255],
+  [255, 0, 0],
+  [255, 173, 74],
+  [255, 103, 52],
+];
 
 function readFile(input) {
   let file = input.files[0];
-
   let reader = new FileReader();
 
   reader.readAsText(file);
 
   reader.onload = function () {
-    dataJSON = JSON.parse(reader.result);
-    method = Object.keys(dataJSON)[0];
-    a = 0;
-    trajectoireVisitee = [];
+    let dataJSON = JSON.parse(reader.result);
+    let method = Object.keys(dataJSON)[0];
 
-    console.log(dataJSON[method]);
-    for (let i = 0; i < 3750; i++) {
-      point(
-        width / 2 + dataJSON[method][i][0][0] / echelle,
-        height / 2 + dataJSON[method][i][0][1] / echelle,
-      );
-      i++;
-    }
-    stroke(255, 0, 0);
-    fill(0, 100, 200);
-    circle(
-      width / 2 + dataJSON[method][0][0][0] / echelle,
-      height / 2 + dataJSON[method][0][0][1] / echelle,
-      10,
-    );
+    planetes.push({
+      traj: dataJSON[method],
+      couleur: couleurs[planetes.length % couleurs.length],
+      historique: [],
+    });
+    background(0);
+    redraw();
   };
 
   reader.onerror = function () {
     console.log(reader.error);
   };
+}
+
+function preload() {
+  button = createButton("reset");
+  button.mousePressed(reset);
+}
+
+function reset() {
+  a = 0;
+  planetes = [];
+  background(0);
+  redraw();
 }
 
 function mousePressed() {
@@ -50,44 +57,37 @@ function mouseReleased() {
   noLoop();
 }
 
-function windowResized() {
-  createCanvas(windowWidth, windowHeight - 150);
-}
-
 function setup() {
   noLoop();
-  createCanvas(1380, 720);
-  stroke(255, 255, 0);
-  fill(255, 255, 0);
-  circle(width / 2, height / 2, 30);
+  createCanvas(1400, 700);
+  background(0);
   a = 0;
 }
 
 function draw() {
   background(0);
 
-  stroke(255, 255, 255);
-  for (let i = 0; i < trajectoireVisitee.length; i++) {
-    point(trajectoireVisitee[i].x, trajectoireVisitee[i].y);
-  }
-
-  stroke(255, 255, 0);
+  noStroke();
   fill(255, 255, 0);
   circle(width / 2, height / 2, 30);
 
-  let px = width / 2 + dataJSON[method][a][0][0] / echelle;
-  let py = height / 2 + dataJSON[method][a][0][1] / echelle;
+  for (let p of planetes) {
+    let idx = a % p.traj.length;
+    let [r, g, b] = p.couleur;
+    let x = width / 2 + p.traj[idx][0][0] / echelle;
+    let y = height / 2 + p.traj[idx][0][1] / echelle;
 
-  // Ajoute la position actuelle à la trajectoire
-  trajectoireVisitee.push({ x: px, y: py });
+    p.historique.push([x, y]);
 
-  stroke(255, 0, 0);
-  fill(0, 100, 200);
-  circle(px, py, 10);
+    noStroke();
+    fill(r, g, b);
+    for (let pos of p.historique) {
+      circle(pos[0], pos[1], 2);
+    }
 
-  if (a < 36501) {
-    a = a + 5;
-  } else {
-    a = 0;
-  } // repart au début sans effacer la trajectoire
+    fill(r, g, b);
+    circle(x, y, 6);
+  }
+
+  a = a + 5;
 }
