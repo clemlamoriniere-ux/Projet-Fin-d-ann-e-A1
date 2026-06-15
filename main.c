ea1b33317a6849f84383f12ebea07fdf1461df31
ItWas271k <ethan.boudjoudi@gmail.com>
2026-06-15 15:36:10 +0200

    normalement fonctionnel

--- main.c
+++ main.c
@@ -23,9 +23,9 @@ int main(){
     trajectoire traj = new_trajectoire(365); // Révolution de la planète
     double masse;
     double perihelie;
-    char* liste_planete[8] = {"mercure","venus","mars","terre","jupiter","saturne","uranus","neptune"};
+    char* liste_planete[6] = {"mercure","venus","mars","terre","jupiter","saturne"}; // uranus et neptune trop loin
     if(chaine == "*") // toutes les planètes
-        for(int i=0;i<8;i++)
+        for(int i=0;i<6;i++)
             creer_fichier(new_planete(liste_planete[i],masse,traj,perihelie));
     else{
         creer_fichier(new_planete(chaine,masse,traj,perihelie));
